f28bc3565d191d219edb61896047db0fcb8eba24
Dmitriy Sokolov <codemitry@gmail.com>
2025-06-24 17:52:26 +0300

    - added p10k - setup for zsh - oh my posh git more verbose

--- setup.sh
+++ setup.sh
@@ -1,15 +1,42 @@
-root=$(dirname "$0")
+#!/bin/bash
 
-setup_wezterm=$skip_wezterm && (!($setup_wezterm || $setup_neovim || $setup_ohmyposh || $setup_powershell) || $setup_wezterm)
-setup_zsh=$skip_zsh && (!($setup_wezterm || $setup_neovim || $setup_ohmyposh || $setup_powershell) || $setup_zsh)
+homedir=$(eval echo ~$USER)
+root=$(dirname $(realpath "$0"))
 
-if [ $setup_wezterm ]; then
-    ln -s "$root/wezterm/wezterm.lua" "~/.wezterm.lua"
-    ln -s "$root/wezterm/.wezterm" "~/.wezterm"
-    ln -s "$root/utilua" "~/.wezterm/dotlocal/utilua"
+setup_wezterm_init=$setup_wezterm
+setup_zsh_init=$setup_zsh
+
+[[ !($skip_wezterm) && !($setup_wezterm_init || $setup_neovim_init || $setup_ohmyposh_init || $setup_zsh_init) || $setup_wezterm_init ]] &&
+setup_wezterm=true || setup_wezterm=false
+[[ !($skip_zsh) && !($setup_wezterm_init || $setup_neovim_init || $setup_ohmyposh_init || $setup_zsh_init) || $setup_zsh_init ]] && setup_zs
+h=true || setup_zsh=false
+
+echo "wezterm.lua: $root/wezterm/wezterm.lua"
+echo "homedir: $homedir"
+if [[ $setup_wezterm == true ]]; then
+    echo "Setup wezterm"
+    ln -s "$root/wezterm/wezterm.lua" "$homedir/.wezterm.lua"
+    ln -s "$root/wezterm/.wezterm" "$homedir/.wezterm"
+    # mkdir -p "$homedir/.wezterm/dotlocal"
+    ln -s "$root/utilua" "$homedir/.wezterm/dotlocal/utilua"
 fi
 
 if [ $setup_zsh ]; then
-    ln -s "$root/zsh/zshrc" "~/.zshrc"
-    ln -s "$root/zsh/.zsh" "~/.zsh"
+    echo "Setup zsh"
+    # complete manually
+    # - zsh:
+    #   - sudo apt install zsh
+    # - oh-my-zsh:
+    #   - sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # - zsh-autocomplete:
+    #   - git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+    # - zsh-syntax-highlighting:
+    #   - git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    # - zsh-fast-syntax-highlighting:
+    #   - git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+    # - zsh-autocomplete
+    #   - git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    ln -s "$root/zsh/zshrc" "$homedir/.zshrc"
+    ln -s "$root/zsh/.zsh" "$homedir/.zsh"
+    ln -s "$root/zsh/p10k.zsh" "$homedir/.p10k.zsh"
 fi
