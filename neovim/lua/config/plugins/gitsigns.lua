216cf2cba9c0b11aaaf09e6538bb43fc4dc08e8e
Dmitriy Sokolov <codemitriy@gmail.com>
2025-07-14 13:21:53 +0300

    - navigate between even staged hunks - extend gitgitnore

--- neovim/lua/config/plugins/gitsigns.lua
+++ neovim/lua/config/plugins/gitsigns.lua
@@ -12,18 +12,18 @@ return {
             if vim.wo.diff then
                 vim.cmd.normal({ "]c", bang = true })
             else
-                gs.nav_hunk("next")
+                gs.next_hunk({ target = 'all' })
             end
         end, "Next Hunk")
         map("n", "[h", function()
             if vim.wo.diff then
                 vim.cmd.normal({ "[c", bang = true })
             else
-                gs.nav_hunk("prev")
+                gs.prev_hunk({ target = 'all' })
             end
         end, "Prev Hunk")
-        map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
-        map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
+        map("n", "]H", function() gs.nav_hunk("last", { target = 'all' }) end, "Last Hunk")
+        map("n", "[H", function() gs.nav_hunk("first", , { target = 'all' }) end, "First Hunk")
         map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
         map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
         map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
