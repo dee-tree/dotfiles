7d22b2527b0a406a522e2d334c7a2e26828f1471
dmitriy sokolov <codemitry@gmail.com>
2025-10-13 15:53:00 +0300

    git log abbrev for fish

--- fish/.fish/aliases.fish
+++ fish/.fish/aliases.fish
@@ -3,4 +3,6 @@ function take --description "Create directory and go inside"
     mkdir -p "$argv[1]"; and cd "$argv[1]"
 end
 
+abbr --add gitlog 'git log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)" --all'
+
 funcsave take
